064da303305d5f26ec9f0f86269e1554b6956746
shukina7olga <olga.shchukina.99@mail.ru>
2020-06-13 23:33:12 +0300

    В большей степени работает. НО первое условие на нажатие отмена не проверяет

--- js/script.js
+++ js/script.js
@@ -1,22 +1,40 @@
-'use strict';
+'use strict'; 
 const numBot = function() {
-    let n = 5;
-    let num = prompt('Угадай число от 1 до 100');
-    if(num === false) {
-        alert('Игра окончена');
-        numBot();
-    } else if(num > n) {
-        confirm('Загаданное число меньше');
-        numBot();
-    } else if(num < n) {
-        confirm('Загаданное число больше');
-        numBot();
-    } else if(!isNaN(num) === true) {
-        confirm('Введи число!');
-        numBot();
-    } 
-    numBot();
+    let n = Math.floor(Math.random() * 10 + 1);
+    console.log('numBot', n, typeof n);
+    const numBotInto = function() {
+        let num = +prompt('Угадай число от 1 до 100');
+        console.log('numBotInto', num, typeof num);
+        if(num === null) {
+            return; 
+        }
+        if(isNaN(num) === true) {
+            let f = confirm('Введи число!');
+            if(f === false) {
+                return; 
+            }
+            numBotInto();
+        } else if(n < num) {
+            let f = confirm('Загаданное число меньше');
+            if(f === false) {
+                return; 
+            }
+            numBotInto();
+        } else if(n > num) {
+            let f = confirm('Загаданное число больше');
+            if(f === false) {
+                return; 
+            }
+            numBotInto();
+        } else if(n === num) {
+            alert('Угадали, теперь вас возьмут на работу в гугл!');
+            return;
+        }   
+    };
+    numBotInto();
 };
 numBot();
 
-
+/*
+let s = prompt('Введи число!'); // +-confirm       undefined  alert     null prompt  
+console.log(s); */
